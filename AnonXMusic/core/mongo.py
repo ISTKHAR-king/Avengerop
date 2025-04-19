66397789b178ef4a0b88588a107aa064f640fd31
Tosu <167279662+about-tosu@users.noreply.github.com>
2025-04-20 03:03:35 +0530

    Update mongo.py

--- AnonXMusic/core/mongo.py
+++ AnonXMusic/core/mongo.py
@@ -8,18 +8,6 @@ try:
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
     mongodb = _mongo_async_.Anon
     LOGGER(__name__).info("Connected to your Mongo Database.")
-
-# Get chats and users collections
-    chats_collection = mongodb["chats"]
-    users_collection = mongodb["tgusersdb"]
-
-# Count documents
-    chats_count = await chats_collection.count_documents({})
-    users_count = await users_collection.count_documents({})
-
-    LOGGER(__name__).info(f"Total Migrated Chats: {chats_count}")
-    LOGGER(__name__).info(f"Total Migrated Users: {users_count}")
-
 except:
     LOGGER(__name__).error(f"Failed to connect to your Mongo Database. Error: {e}")
     exit()
