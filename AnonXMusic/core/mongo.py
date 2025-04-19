88343fd875f82da24aad6ea008f88e4e428a51f4
Tosu <167279662+about-tosu@users.noreply.github.com>
2025-04-20 02:59:42 +0530

    Update mongo.py

--- AnonXMusic/core/mongo.py
+++ AnonXMusic/core/mongo.py
@@ -3,24 +3,23 @@ from config import MONGO_DB_URI
 from ..logging import LOGGER
 import asyncio
 
-logger = LOGGER(__name__)
-       logger.info("Connecting to your Mongo Database...")
-    try:
-        _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
-        mongodb = _mongo_async_.Anon
-        logger.info("Connected to your Mongo Database.")
+LOGGER(__name__).info("Connecting to your Mongo Database...")
+try:
+    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
+    mongodb = _mongo_async_.Anon
+    LOGGER(__name__)info("Connected to your Mongo Database.")
 
-        # Get chats and users collections
-        chats_collection = mongodb["chats"]
-        users_collection = mongodb["tgusersdb"]
+# Get chats and users collections
+    chats_collection = mongodb["chats"]
+    users_collection = mongodb["tgusersdb"]
 
-        # Count documents
-        chats_count = await chats_collection.count_documents({})
-        users_count = await users_collection.count_documents({})
+# Count documents
+    chats_count = await chats_collection.count_documents({})
+    users_count = await users_collection.count_documents({})
 
-        logger.info(f"Total Migrated Chats: {chats_count}")
-        logger.info(f"Total Migrated Users: {users_count}")
+    LOGGER(__name__).info(f"Total Migrated Chats: {chats_count}")
+    LOGGER(__name__).info(f"Total Migrated Users: {users_count}")
 
-    except Exception as e:
-        logger.error(f"Failed to connect to your Mongo Database. Error: {e}")
-        exit()
+except:
+    LOGGER(__name__).error(f"Failed to connect to your Mongo Database. Error: {e}")
+    exit()
